b63dfe8d524bf1409400d2b4a6b0b3b920ed693c
niklaus3016 <Jp112022@163.com>
2026-05-21 08:16:22 +0000

    Fix capacitor.config.ts: remove unnecessary server config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,10 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.kuaixuewm.app',
   appName: '快序二维码',
   webDir: 'dist',
-  server: {
-    androidScheme: 'https'
-  }
+  android: {}
 };
 
 export default config;
